c5cc586be12f2b4d6c0fb9a1569b7e550d5db068
Viliam Valent <vilo.valent@gmail.com>
2018-04-09 19:45:24 +0200

    Linted using Black

--- task.py
+++ task.py
@@ -5,7 +5,6 @@ from dateutil.relativedelta import relativedelta
 from dateutil.parser import parse
 from datetime import datetime, date
 
-
 default = {
     "project": None,
     "tasks": None,
@@ -15,102 +14,125 @@ default = {
     "repeat": None,
     "repeat_index": None,
     "index": None,
-    "priority": todoist.Priority.NORMAL
+    "priority": todoist.Priority.NORMAL,
 }
 
 
 def string_to_relativedelta(s, num_pos=0, text_pos=1):
     if s is None:
         return None
-    interval = [0, 0, 0, 0] # years, months, weeks, days
-    line = s.strip().split(' ')
+
+    interval = [0, 0, 0, 0]  # years, months, weeks, days
+    line = s.strip().split(" ")
     number = int(line[num_pos])
     text = line[text_pos][0]
-    if text in 'Dd':
+    if text in "Dd":
         interval[3] = number
-    elif text in 'Ww':
+    elif text in "Ww":
         interval[2] = number
-    elif text in 'Mm':
+    elif text in "Mm":
         if len(line) > 2:
             # last day of month
             interval[1] = number
             interval[3] = -1
         else:
             interval[1] = number
-    elif text in 'Yy':
+    elif text in "Yy":
         interval[0] = number
-    elif text in 'Ll':
+    elif text in "Ll":
         interval[1] = number
         interval[3] = -1
-    return relativedelta(years=interval[0],
-                         months=interval[1],
-                         weeks=interval[2],
-                         days=interval[3])
+    return relativedelta(
+        years=interval[0], months=interval[1], weeks=interval[2], days=interval[3]
+    )
+
 
 def old_to_new_format(path):
     for filename in os.listdir(path):
-        with open(f'{path}/{filename}') as f:
+        with open(f"{path}/{filename}") as f:
             d = default.copy()
-            d['tasks'] = []
+            d["tasks"] = []
             for line in f:
-                if line[0] in 'Pp':
+                if line[0] in "Pp":
                     d["project"] = line.strip()[2:]
-                elif line[0] in 'Ii':
+                elif line[0] in "Ii":
                     d["interval"] = line.strip()[2:]
-                elif line[0] in 'Tt':
-                    d['tasks'].append(line.strip()[2:])
-                elif line[0] in 'Cc':
-                    d['index'] = int(line.strip().split(' ')[1])
-                elif line[0] in 'Dd':
-                    d['due_date'] = line.strip()[2:]
-                elif line[0] in 'Ee':
-                    d['early'] = line.strip()[2:]
-        new_filename = filename.strip().split('.')[0]
-        toml.dump(d, open(f'{path}/{new_filename}.toml', 'w'))
+                elif line[0] in "Tt":
+                    d["tasks"].append(line.strip()[2:])
+                elif line[0] in "Cc":
+                    d["index"] = int(line.strip().split(" ")[1])
+                elif line[0] in "Dd":
+                    d["due_date"] = line.strip()[2:]
+                elif line[0] in "Ee":
+                    d["early"] = line.strip()[2:]
+        new_filename = filename.strip().split(".")[0]
+        toml.dump(d, open(f"{path}/{new_filename}.toml", "w"))
+
 
 def write(task, filename, verbose):
-    toml.dump(task, open(filename, 'w'))
-    if verbose: print(f'-> Changes written to file {filename.split("/")[-1]}.', flush=True)
+    toml.dump(task, open(filename, "w"))
+    if verbose:
+        print(f'-> Changes written to file {filename.split("/")[-1]}.', flush=True)
+
 
 def from_file(filename):
     task = default.copy()
     task.update(toml.load(filename))
     return task
 
+
 def delete(task, filename, verbose):
     os.remove(filename)
-    if verbose: print(f'-> File {filename.split("/")[-1]} deleted.', flush=True)
+    if verbose:
+        print(f'-> File {filename.split("/")[-1]} deleted.', flush=True)
+
 
 def execute(task, user, verbose, filename, frontload=0):
-    due_date = parse(task['due_date']).date()
-    early = string_to_relativedelta(task['early'])
+    due_date = parse(task["due_date"]).date()
+    early = string_to_relativedelta(task["early"])
     todoist_project = None
     new_task = task.copy()
     rewrite = False
-    interval = string_to_relativedelta(task['interval'])
+    interval = string_to_relativedelta(task["interval"])
     while date.today() + relativedelta(days=frontload) >= due_date - early:
         if todoist_project is None:
-            todoist_project = user.get_project(task['project'])
-        if task['interval'] is None:
+            todoist_project = user.get_project(task["project"])
+        if task["interval"] is None:
             # One time task
-            for t in task['tasks']:
-                todoist_project.add_task(t, date=task['due_date'], priority=task['priority'])
-                if verbose: print('-> Added new task \'{}\' with due date {}.'
-                        .format(t, task['due_date']), flush=True)
+            for t in task["tasks"]:
+                todoist_project.add_task(
+                    t, date=task["due_date"], priority=task["priority"]
+                )
+                if verbose:
+                    print(
+                        "-> Added new task '{}' with due date {}.".format(
+                            t, task["due_date"]
+                        ),
+                        flush=True,
+                    )
             delete(task, filename, verbose)
             break
+
         else:
             # Recurring task
-            todoist_project.add_task(new_task['tasks'][new_task['index']],
-                                     date=new_task['due_date'], priority=task['priority'])
-            if verbose: print('-> Added new task \'{}\' with due date {}.'
-                    .format(new_task['tasks'][new_task['index']], new_task['due_date']), flush=True)
+            todoist_project.add_task(
+                new_task["tasks"][new_task["index"]],
+                date=new_task["due_date"],
+                priority=task["priority"],
+            )
+            if verbose:
+                print(
+                    "-> Added new task '{}' with due date {}.".format(
+                        new_task["tasks"][new_task["index"]], new_task["due_date"]
+                    ),
+                    flush=True,
+                )
             # incrementing values
-            if interval.days == -1: # last day of month
+            if interval.days == -1:  # last day of month
                 due_date += relativedelta(days=+1)
             due_date += interval
-            new_task['due_date'] = due_date.isoformat()
-            new_task['index'] = (new_task['index'] + 1) % len(new_task['tasks'])
+            new_task["due_date"] = due_date.isoformat()
+            new_task["index"] = (new_task["index"] + 1) % len(new_task["tasks"])
             rewrite = True
-    if rewrite: write(new_task, filename, verbose)
-
+    if rewrite:
+        write(new_task, filename, verbose)
--- todoist_scheduler.py
+++ todoist_scheduler.py
@@ -6,62 +6,78 @@ import argparse
 import pickle
 import toml
 
-parser = argparse.ArgumentParser(description='''Todoist Scheduler can store future one-time or recurring tasks for Todoist \n
+parser = argparse.ArgumentParser(
+    description="""Todoist Scheduler can store future one-time or recurring tasks for Todoist \n
                                                 in plain text and create a task in Todoist when they are needed. \n
-                                                Also offers more features regarding a set of repeating tasks.''')
-parser.add_argument('-f', dest='frontload', type=int, default=0,
-                        help='Useful when you are going to be away from computer for X days. Use X as a parameter.')
-parser.add_argument('-v', dest='verbose', action='store_true',
-                        help='Verbose output.')
-parser.add_argument('--first', dest='first_start', action='store_true',
-                        help='First start of a script. Creates login information file and default config.')
+                                                Also offers more features regarding a set of repeating tasks."""
+)
+parser.add_argument(
+    "-f",
+    dest="frontload",
+    type=int,
+    default=0,
+    help="Useful when you are going to be away from computer for X days. Use X as a parameter.",
+)
+parser.add_argument("-v", dest="verbose", action="store_true", help="Verbose output.")
+parser.add_argument(
+    "--first",
+    dest="first_start",
+    action="store_true",
+    help="First start of a script. Creates login information file and default config.",
+)
 
 frontload = parser.parse_args().frontload
 verbose = parser.parse_args().verbose
 directory = os.path.dirname(os.path.realpath(__file__))
 if parser.parse_args().first_start:
     working_login = False
-    print('This is a Todoist Scheduler. Please enter your Todoist email and password.')
-    print('They will be stored unencrypted as Python Pickle file so do not share the file \'login\'')
+    print("This is a Todoist Scheduler. Please enter your Todoist email and password.")
+    print(
+        "They will be stored unencrypted as Python Pickle file so do not share the file 'login'"
+    )
     while not working_login:
-        email = input('Email: ')
-        password = input('Password: ')
+        email = input("Email: ")
+        password = input("Password: ")
         try:
             todoist.login(email, password)
             working_login = True
-            print('Login successful.')
+            print("Login successful.")
         except:
-            print('Login unsuccessful. Please, try again.')
-    pickle.dump((email, password), open(directory + '/login', 'wb'))
-    dir = directory + '/tasks'
+            print("Login unsuccessful. Please, try again.")
+    pickle.dump((email, password), open(directory + "/login", "wb"))
+    dir = directory + "/tasks"
     if not os.path.exists(dir):
         os.makedirs(dir)
-    print('Creating default configuration file todoist_scheduler.conf.  Feel free to change it.')
-    conf = {"login": directory + '/login', "tasks_directory": dir}
-    toml.dump(conf, open(f'{directory}/todoist_scheduler.conf', 'w'))
-    print('By default, tasks will be stored in directory:\n{}.'.format(dir))
+    print(
+        "Creating default configuration file todoist_scheduler.conf.  Feel free to change it."
+    )
+    conf = {"login": directory + "/login", "tasks_directory": dir}
+    toml.dump(conf, open(f"{directory}/todoist_scheduler.conf", "w"))
+    print("By default, tasks will be stored in directory:\n{}.".format(dir))
     exit()
 # normal run
 # load conf
 
-conf = toml.load(f'{directory}/todoist_scheduler.conf')
+conf = toml.load(f"{directory}/todoist_scheduler.conf")
 try:
-    user = todoist.login(*pickle.load(open(conf['login'], 'rb')))
+    user = todoist.login(*pickle.load(open(conf["login"], "rb")))
 except:
     try:
         from gi.repository import Notify
-        Notify.init('Todoist scheduler')
-        notification = Notify.Notification.new('Unable to log in Todoist')
+
+        Notify.init("Todoist scheduler")
+        notification = Notify.Notification.new("Unable to log in Todoist")
         notification.set_urgency(2)
         notification.show()
     except:
         pass
     finally:
         exit()
-for f in os.listdir(conf['tasks_directory']):
-    if '.toml' in f or '.TOML' in f:
-        if verbose: print('Dealing with task "{}".'.format(f.split('.')[0]))
-        filename = "{}/{}".format(conf['tasks_directory'], f)
+for f in os.listdir(conf["tasks_directory"]):
+    if ".toml" in f or ".TOML" in f:
+        if verbose:
+            print('Dealing with task "{}".'.format(f.split(".")[0]))
+        filename = "{}/{}".format(conf["tasks_directory"], f)
         with open(filename) as ff:
             task = from_file(filename)
             execute(task, user, verbose, filename, frontload)
